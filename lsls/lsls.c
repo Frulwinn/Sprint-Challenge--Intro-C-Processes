3a1935d11a6d8de0384789ff5337a638e5bc3a29
Frulwinn <frulwinn@gmail.com>
2019-07-19 14:10:39 -0500

    challenge

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * Main
+ * If you want to test if your environment is set up correctly, 
+ * compile and run the [testdir.c](examples/testdir.c) 
+ * program in the `examples/` directory. 
+ * (You can type `make` in the `examples/` directory.) 
+ * It should print `Testing: PASS`.
  */
+
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *path;
+  if (argc > 1) {
+    path = argv[1];
+    fprintf(stderr, "Extra args. \n");
+    exit(0);
+  } else {
+    path = ".";
+  }
 
   // Open directory
+  DIR *directory = opendir(path);
+  if (directory == NULL) {
+    fprintf(stderr, "Cannot open directory.\n");
+    exit (1);
+  }
 
   // Repeatly read and print entries
+  struct dirent *entries;
+  struct stat buf;
+
+    while ((entries = readdir(directory)) != NULL) {
+      char *file_name = entries->d_name;
+      stat(file_name, &buf);
+      printf("%10lld  %s\n", buf.st_size, file_name);
+  }
 
   // Close directory
-
+  closedir(directory);
   return 0;
 }
